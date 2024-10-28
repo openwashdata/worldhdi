e935a3d43f345d1203d18438c2b0b9cf25fd0d2f
yashdubey132 <yashdubey1321998@gmail.com>
2024-10-28 15:59:46 +0100

    Updated data_processing, added readme and roxygen

--- data-raw/data_processing.R
+++ data-raw/data_processing.R
@@ -8,17 +8,68 @@ library(fs)
 library(here)
 library(readr)
 library(openxlsx)
+library(tidyverse)
+library(countrycode)
 
 # Read data --------------------------------------------------------------------
-# data_in <- read_csv("data-raw/dataset.csv")
-# codebook <- read_excel("data-raw/codebook.xlsx") |>
-#  clean_names()
-
+data_in <- read.xlsx("HDR23-24_Statistical_Annex_HDI_Trends_Table.xlsx", startRow=5,
+                     skipEmptyCols=TRUE)
 # Tidy data --------------------------------------------------------------------
 ## Clean the raw data into a tidy format here
+# Update column names
+worldhdi <- data_in |>
+  rename(
+    hdi_rank = "HDI.rank",
+    country = "Country",
+    hdi_1990 = "1990",
+    hdi_2000 = "2000",
+    hdi_2010 = "2010",
+    hdi_2015 = "2015",
+    hdi_2019 = "2019",
+    hdi_2020 = "2020",
+    hdi_2021 = "2021",
+    hdi_2022 = "2022",
+    rank_change_2015_2022 = "2015-2022",
+    avg_growth_1990_2000 = "1990-2000",
+    avg_growth_2000_2010 = "2000-2010",
+    avg_growth_2010_2022 = "2010-2022",
+    avg_growth_1990_2022 = "1990-2022"
+  )
+
+# Drop unused columns
+worldhdi <- worldhdi |>
+  select(!(c("X4", "a")))
+
+# Remove rows that separate countries according to HDI tiers
+worldhdi <- worldhdi |>
+  filter(if_any(-country, ~ !is.na(.)))
+
+# Replace .. with NA
+worldhdi <- worldhdi |>
+  mutate(across(everything(), ~ ifelse(. == "..", NA, .)))
+
+# Add HDI tiers as a column (Very High (0.8 - 1), High (0.7 - 0.799), Medium (0.55 - 0.699), Low (0 - 0.549)
+worldhdi <- worldhdi |>
+  mutate(tier_hdi = case_when(
+    hdi_2022 >= 0.8 ~ "Very High",
+    hdi_2022 >= 0.7 & hdi_2022 < 0.8 ~ "High",
+    hdi_2022 >= 0.55 & hdi_2022 < 0.7 ~ "Medium",
+    hdi_2022 < 0.55 ~ "Low",
+    is.na(hdi_2022) ~ NA_character_
+  ))
 
 
-# Export Data ------------------------------------------------------------------
+# Add iso3c column
+worldhdi <- worldhdi |>
+  mutate(iso3c = countrycode::countrycode(country, origin = "country.name", destination = "iso3c"))
+
+worldhdi <- worldhdi |>
+  mutate(across(
+    .cols = -c(country, iso3c, tier_hdi),
+    .fns = as.numeric
+  ))
+
+# Export data ------------------------------------------------------------------
 usethis::use_data(worldhdi, overwrite = TRUE)
 fs::dir_create(here::here("inst", "extdata"))
 readr::write_csv(worldhdi,
